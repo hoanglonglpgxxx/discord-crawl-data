2c7a82ca87ed724d35ae40e4ed53997e550a1829
Mits <hoanglonglpgxxx@gmail.com>
2024-11-09 17:41:58 +0700

    asdasd

--- function_app.py
+++ function_app.py
@@ -6,15 +6,15 @@ import datetime
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.timer_trigger(schedule="30 7 * * *", arg_name="myTimer", run_on_startup=False,
-              use_monitor=False) 
+@app.function_name(name="daily")
+@app.timer_trigger(schedule="0 */10 * * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
 def daily(myTimer: func.TimerRequest) -> None:
     
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
     # Fetch news data from the API
-    url = f'https://newsdata.io/api/1/latest?country=vi&category=politics&apikey={os.getenv("NEWSDATA_KEY")}'
+    url = 'https://newsdata.io/api/1/latest?country=vi&category=politics&apikey=pub_58695ffe53513b97d78ce05437d91b5109031'
     response = requests.get(url)
 
     # Check if the request was successful
@@ -27,13 +27,13 @@ def daily(myTimer: func.TimerRequest) -> None:
 
     # Format message with fetched news data
     msg = f"**Today's Hot News - {datetime.datetime.now().strftime('%Y-%m-%d')}**\n\n"
-    for result in news_list:
+    for result in news_list[:5]:  # Limiting to the first 5 articles for brevity
         title = result.get('title', 'No title')
         link = result.get('link', 'No link')
         msg += f"**Tiêu đề**: {title}\n**Link**: {link}\n\n"
 
     # Send message to Discord
-    discord_webhook_url = os.getenv('DISCORD_WEBHOOK_URL')  # Set your Discord webhook URL in environment variables
+    discord_webhook_url = 'https://discord.com/api/webhooks/1304729546934980668/RurMeu02XAOVJIET1xOc-AJSksNXC6pa5jRV9U_-kB5XwLu-TcMW-9pPZDFL0UBGOe1v'
     discord_message = {"content": msg}
 
     discord_response = requests.post(discord_webhook_url, json=discord_message)
@@ -41,4 +41,3 @@ def daily(myTimer: func.TimerRequest) -> None:
         print("News sent successfully to Discord")
     else:
         print(f"Failed to send news to Discord. Status code: {discord_response.status_code}")
-    # Call the main function from __init__.py
